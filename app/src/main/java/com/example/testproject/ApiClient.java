e4b594cf53da42e52aeab91e30a2031bc6d84444
CyLux <vlad.samohvalov45@gmail.com>
2020-12-03 17:44:57 +0900

    123

---
+++ app/src/main/java/com/example/testproject/ApiClient.java
@@ -0,0 +1,24 @@
+package com.example.testproject;
+import okhttp3.OkHttpClient;
+
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static Retrofit getRetrofit(){
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.level(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(logging).build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("http://api.larntech.net/")
+                .client(okHttpClient)
+                .build();
+        return retrofit;
+    }
+    public static UserService getUserServices(){
+        UserService userService = getRetrofit().create(UserService.class);
+        return userService;
+    }
+}
--- app/src/main/java/com/example/testproject/SignIn.java
+++ app/src/main/java/com/example/testproject/SignIn.java
@@ -2,13 +2,28 @@ package com.example.testproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SignIn extends AppCompatActivity {
+public class SignIn extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+        TextView Signup = (TextView) findViewById(R.id.create);
+        Signup.setOnClickListener(this);
+    }
+    public void onClick(View view) {
+        Intent i;
+        i = new Intent(this, Signup.class);
+        startActivity(i);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
