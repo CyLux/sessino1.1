acdd37f9862eb04f08e92c371eeeeb0533fd3620
CyLux <vlad.samohvalov45@gmail.com>
2020-12-12 22:54:18 +0900

    123

--- app/src/main/java/com/example/testproject/StartScreen.java
+++ app/src/main/java/com/example/testproject/StartScreen.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class  StartScreen extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
+        Fragment fragment = new MapFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout,fragment)
+                .commit();
     }
 }
