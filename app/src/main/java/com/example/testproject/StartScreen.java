b3b5180884bc33218ed77fc8b57cf174e528b6dd
CyLux <vlad.samohvalov45@gmail.com>
2020-12-12 22:10:50 +0900

    123

---
+++ app/src/main/java/com/example/testproject/StartScreen.java
@@ -0,0 +1,13 @@
+package com.example.testproject;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class  StartScreen extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_screen);
+    }
+}
