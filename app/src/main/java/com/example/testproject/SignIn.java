e4fc200fca73ba6e386de7d4acde99cbd191014f
CyLux <vlad.samohvalov45@gmail.com>
2020-12-12 22:10:26 +0900

    123

--- app/src/main/java/com/example/testproject/SignIn.java
+++ app/src/main/java/com/example/testproject/SignIn.java
@@ -1,11 +1,13 @@
 package com.example.testproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignIn extends AppCompatActivity implements View.OnClickListener {
 
@@ -21,9 +23,26 @@ public class SignIn extends AppCompatActivity implements View.OnClickListener {
         i = new Intent(this, Signup.class);
         startActivity(i);
     }
-
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
+    public void onClickVhod(View v) {
+        EditText login = (EditText)findViewById(R.id.edUsername);
+        EditText password = (EditText)findViewById(R.id.edPassword);
+
+
+        if(login.getText().toString().equals("serk") && password.getText().toString().equals("123")){
+            Intent o = new Intent(SignIn.this, StartScreen.class);
+            startActivity(o);
+            finish();
+
+        }
+        else
+        {
+            Toast.makeText(this, "Неправильно введен пароль или логин", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
